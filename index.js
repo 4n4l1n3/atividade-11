995eb2067b1e11d2b81d577373340abee4546b82
4n4l1n3 <132668983+4n4l1n3@users.noreply.github.com>
2023-07-28 13:14:19 +0000

    Adicionando arquivos

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";      
+// Requisição do pacote do express
+const app = express();              
+// Instancia o Express
+const port = 3000;                  
+// Define a porta
+
+app.get("/", (req, res) => {        
+    // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Analine Almeida e Silva",      
+    // Substitua pelo seu nome
+  });
+  console.log("Rota / solicitada");
+});
+
+app.listen(port, () => {            
+    // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
