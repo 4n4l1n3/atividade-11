5b13b92d2a256ec3c4bb40b25f093865108077dc
4n4l1n3 <132668983+4n4l1n3@users.noreply.github.com>
2023-08-31 13:16:25 +0000

    atividade 14

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import roteadorUsuario from "./routes/usuario.js";
 import dotenv from "dotenv";
-dotenv.config();
-//import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";    
+import express from "express";     
 
 const app = express();              
 const port = 3000;                  
+app.use(roteadorUsuario);
+dotenv.config();
 
 app.get("/", (req, res) => {        
   res.json({
@@ -17,6 +18,6 @@ app.listen(port, () => {
   console.log(`Serviço escutando na porta:  ${port}`);
 });
 
-app.use(roteadorUsuario);import express from "express"; 
+
 
 
